a24ab6ad5b9b73795d43f718179e03cf82a15883
Gabriel França <anonimato5592@gmail.com>
2023-10-11 10:33:12 -0300

    Added explanatory comments in the code
    Added explanatory comments in English for each function in the code.
    Imported external data from names and surnames files.
    The getRandomItem function returns a random item from an array.
    The formatIsoCode function formats the ISO code to lowercase.
    The getRandomName function returns a random name based on ISO code and gender.
    The getRandomSurname function returns a random surname based on ISO code.
    The firstName function generates one or more first names.
    The lastName function generates one or more last names.
    The fullName function generates a complete name with a specified number of first and last names.

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,18 @@
+// Importing data arrays from external files
 import { names } from './names';
 import { surnames } from './surnames';
 
+// Function to get a random item from an array
 const getRandomItem = (array: string[]): string => {
   return array[Math.floor(Math.random() * array.length)];
 };
 
+// Function to format ISO code to lowercase
 const formatIsoCode = (isoCode: string) => {
   return isoCode.toLowerCase();
 }
 
+// Function to get a random name based on ISO code and gender
 const getRandomName = (isoCode: string, gender: Gender): string => {
   isoCode = formatIsoCode(isoCode);
   if (!names[isoCode] || !names[isoCode][gender]) {
@@ -17,6 +21,7 @@ const getRandomName = (isoCode: string, gender: Gender): string => {
   return getRandomItem(names[isoCode][gender]);
 };
 
+// Function to get a random surname based on ISO code
 const getRandomSurname = (isoCode: string): string => {
   isoCode = formatIsoCode(isoCode);
   if (!surnames[isoCode]) {
@@ -25,6 +30,7 @@ const getRandomSurname = (isoCode: string): string => {
   return getRandomItem(surnames[isoCode]);
 };
 
+// Function to generate one or more first names
 const firstName = (isoCode: string, gender: Gender, n: number = 1): string => {
   const namesArray: string[] = [];
   for (let i = 0; i < n; i++) {
@@ -33,6 +39,7 @@ const firstName = (isoCode: string, gender: Gender, n: number = 1): string => {
   return namesArray.join(' ');
 };
 
+// Function to generate one or more last names
 const lastName = (isoCode: string, n: number = 1): string => {
   const surnamesArray: string[] = [];
   for (let i = 0; i < n; i++) {
@@ -41,10 +48,12 @@ const lastName = (isoCode: string, n: number = 1): string => {
   return surnamesArray.join(' ');
 };
 
+// Function to generate a full name with specified number of first and last names
 const fullName = (isoCode: string, gender: Gender, firstN: number= 1, lastN: number = 1, space: string = ' '): string => {
   return `${firstName(isoCode, gender, firstN)}${space}${lastName(isoCode, lastN)}`;
 };
 
+// Exporting the functions for use in other files
 export {
   firstName,
   lastName,
