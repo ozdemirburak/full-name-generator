d07bc72806e29328c8770f311444c625e2ea65d3
Anemaygi <gianemayumisan@gmail.com>
2023-10-14 15:10:33 -0300

    add: Peruvian given names

---
+++ src/names/pe.ts
@@ -0,0 +1,14 @@
+const peruvianNames = {
+  0: [ // https://elcomercio.pe/peru/fiestas-patrias-40-nombres-apellidos-comunes-peru-noticia-540912-noticia/
+    'José', 'Luis', 'Juan', 'Carlos', 'Jorge', 'Víctor', 'César', 'Miguel', 'Jesús', 'Julio',
+    'Pedro', 'Manuel', 'Santos', 'Jhon', 'David', 'Segundo', 'Diego', 'Daniel', 'Óscar',
+    'Marco', 'Edwin', 'Javier', 'Walter', 'Milagros', 'Fernando', 'Alejandro', 'Alex', 'Francisco',
+  ],
+  1: [ // https://elcomercio.pe/peru/fiestas-patrias-40-nombres-apellidos-comunes-peru-noticia-540912-noticia/
+    'María', 'Rosa', 'Ana', 'Luz', 'Carmen', 'Juana', 'Ángel', 'Flor', 'Julia', 'Elizabeth', 'Ruth', 'Diana',
+    'Mia', 'Alessia', 'Camila', 'Danna', 'Luciana', 'Aitana', 'María', 'Briana', 'Ariana', 'Valentina', 
+    'Luana', 'Sofia', 'Emma', 'Antonella', 'Emily', 'Lia',
+  ]
+};
+
+export default peruvianNames;
