d8dc4339f0bbb619a4eb7d9cb0d76cf9424c1ec0
Aditya Phasu <adityaphasu919@gmail.com>
2023-10-15 00:18:37 +0530

    add russian names and surnames

---
+++ src/names/ru.ts
@@ -0,0 +1,18 @@
+const russianNames = {
+    0: [ //https://forebears.io/russia/forenames
+      "Sergey", "Aleksandr", "Andrey", "Dmitriy", "Vladimir", "Aleksey", "Maksim", "Ivan", "Evgeniy", "Alexander",
+      "Nikolay", "Mikhail", "Denis", "Roman", "Pavel", "Igor", "Anton", "Nikita", "Oleg", "Ilya",
+      "Viktor", "Alexey","Konstantin","Artyom", "Kirill", "Yuriy","Dmitry","Ruslan","Vadim", "Vitaliy",
+      "Artem","Evgeny", "Danil","Stanislav","Vyacheslav","Vladislav","Anatoliy","Maxim", "Artur","Daniil",
+      "Gennadiy","Leonid","Vitaly","Rustam", "Boris","Grigoriy", "Anatoly", "Yaroslav","Vasily", "Stepan"
+    ],
+    1: [ //https://forebears.io/russia/forenames
+      "Elena", "Tatyana", "Olga", "Natalya", "Ekaterina", "Irina", "Svetlana", "Anastasiya", "Anna", "Marina",
+      "Mariya", "Yuliya", "Darya", "Nadezhda", "Viktoriya", "Lyudmila", "Galina","Kristina", "Alina", "Kseniya",
+      "Valentina","Lyubov", "Alyona","Aleksandra","Oksana", "Evgeniya", "Anastasia","Yulia", "Elizaveta","Anya",
+      "Valeriya", "Yana", "Olesya", "Yulya", "Larisa","Diana", "Polina", "Vera","Nina", "Alena",
+      "Maria","Margarita","Oxana", "Veronika", "Inna","Karina", "Nataliya","Tamarа", "Angelina","Albina"
+    ],
+  };
+  
+export default russianNames;
---
+++ src/surnames/ru.ts
@@ -0,0 +1,11 @@
+const russianSurnames = [
+    //https://forebears.io/russia/surnames
+   "Ivanova", "Ivanov", "Kuznetsova", "Kuznetsov", "Petrov", "Smirnova", "Magomedov", "Petrova", "Smirnov", "Popov",
+   "Popova", "Volkova", "Novikova", "Morozova", "Sokolova", "Pavlova", "Romanova", "Volkov", "Shevchenko", "Andreeva",
+   "Vasilev", "Vasileva", "Mikhaylova", "Novikov", "Makarova", "Morozov", "Sergeeva", "Pavlov", "Zaytseva", "Sokolov",
+   "Aliev", "Mikhaylov", "Nikolaeva", "Romanov", "Zakharova", "Makarov", "Stepanova", "Lebedeva", "Kozlova", "Nikitina",
+   "Egorova", "Egorov", "Kozlov", "Nikolaev", "Stepanov", "Andreev", "Yakovleva", "Zakharov", "Kovalenko", "Frolova"
+
+];
+ 
+export default russianSurnames;
