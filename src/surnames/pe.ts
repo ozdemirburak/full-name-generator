9eedc94d5a64dbafbd5b4a4a59fc4479b5f862ad
Anemaygi <gianemayumisan@gmail.com>
2023-10-14 15:36:01 -0300

    add: Peruvian Surnames

---
+++ src/surnames/pe.ts
@@ -0,0 +1,8 @@
+const peruvianSurnames = [ // https://elcomercio.pe/peru/fiestas-patrias-40-nombres-apellidos-comunes-peru-noticia-540912-noticia/
+  'López', 'Ramos', 'Pérez', 'Torres', 'Díaz', 'Gonzales', 'Ramírez', 'Mendoza', 'Chávez', 
+  'Espinoza', 'Castillo', 'Fernández', 'Vargas', 'Gutiérrez', 'Cruz', 'Ruiz', 'De la Cruz', 'Romero',
+  'Gómez', 'Silva', 'Córdova', 'Condori', 'Castro', 'Martínez', 'Reyes', 'Rivera', 'Salazar', 'Medina',
+  'Aguilar', 'Morales', 'Paredes',
+];
+
+export default peruvianSurnames;
