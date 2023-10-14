11069914435f1213ef3677928ac1e87597a55551
Ryan Biondo <biondoryan.dev@gmail.com>
2023-10-14 14:10:29 -0600

    Added Spanish names and surnames

---
+++ src/surnames/es.ts
@@ -0,0 +1,9 @@
+const spanishSurnames = [ // Sources: https://www.ine.es/, https://www.rae.es/, https://forebears.io/, https://www.behindthename.com/, https://mydailyspanish.com/, https://www.familyeducation.com/
+    'Alonso', 'Alvarez', 'Blanco', 'Bravo', 'Cabello', 'Calderón', 'Campos', 'Castillo', 'Castro', 'Cruz',
+    'Díaz', 'Domínguez', 'Fernández', 'Flores', 'García', 'Gómez', 'González', 'Guerrero', 'Gutiérrez', 'Hernández',
+    'Jiménez', 'López', 'Martínez', 'Medina', 'Mendoza', 'Molina', 'Montes', 'Moreno', 'Muñoz', 'Navarro',
+    'Nieto', 'Núñez', 'Ortiz', 'Pérez', 'Ramírez', 'Reyes', 'Rivera', 'Rodríguez', 'Romero', 'Ruiz',
+    'Sánchez', 'Santos', 'Silva', 'Soto', 'Suarez', 'Torres', 'Vargas', 'Vázquez', 'Vega', 'Vera'
+];
+
+export default spanishSurnames;
