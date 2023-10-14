bcef1b7106a91aa781fb063ea7fc20980b0f9b55
Uday Sagar <udaysagar.mail@gmail.com>
2023-10-14 23:00:29 +0530

    Added Korean Names and Surnames

---
+++ src/names/ko.ts
@@ -0,0 +1,24 @@
+const koreanNames = {
+    0: [ // https://www.behindthename.com/names/gender/masculine/usage/korean
+        'Byeong-Ho', 'Byung-Ho', 'Dae-Jung', 'Dae-Seong', 'Dong-Geun',
+        'Do-Yun', 'Duri', 'Eun', 'Eun-Jung', 'Eun-U',
+        'Eun-Woo', 'Gyeong', 'Ha-Jun', 'Haneul', 'Hwan',
+        'Hye', 'Hyeon', 'Hyeon-Jeong', 'Hyeon-Ju', 'Hyeon-U',
+        'Hyun', 'Hyun-Joo', 'Hyun-Jung', 'Hyun-Woo', 'Il-Seong',
+        'Iseul', 'Jae', 'Jae-Seong', 'Jeong', 'Jeong-Ho',
+        'Jeong-Hui', 'Jeong-Hun', 'Ji', 'Ji-Ho', 'Ji-Hoon',
+        'Ji-Hu', 'Ji-Hun', 'Ji-Min', 'Ji-Soo', 'Ji-Su',
+        'Ji-U', 'Ji-Won', 'Ji-Woo', 'Ji-Yeong', 'Ji-Young',
+        'Jong', 'Jong-Su', 'Joon', 'Joon-Ho', 'Joo-Won',
+        'Jun'
+    ],
+    1: [ // https://www.behindthename.com/names/gender/feminine/usage/korean
+        'Bora', 'Chae-Won', 'Chae-Yeong', 'Chae-Young', 'Duri', 'Eun', 'Eun-Gyeong', 'Eun-Jeong', 'Eun-Ji', 'Eun-Jung',
+        'Eun-Kyung', 'Eun-U', 'Eun-Woo', 'Eun-Yeong', 'Eun-Young', 'Gyeong', 'Gyeong-Hui', 'Gyeong-Ja', 'Gyeong-Suk', 'Ha-Eun',
+        'Hana', 'Haneul', 'Ha-Yun', 'Hwan', 'Hye-Jin', 'Hyeon', 'Hyeon-Jeong', 'Hyeon-Ju', 'Hyun', 'Hyun-Joo',
+        'Hyun-Jung', 'Iseul', 'Jae', 'Jeong', 'Jeong-Hui', 'Jeong-Suk', 'Ji', 'Ji-Eun', 'Ji-Hu', 'Ji-Hye',
+        'Ji-Min', 'Ji-Soo', 'Ji-Su', 'Ji-U', 'Ji-Won', 'Ji-Woo', 'Ji-Yeon', 'Ji-Yeong', 'Ji-Young', 'Ji-Yu'
+    ]
+}
+
+export default koreanNames;
