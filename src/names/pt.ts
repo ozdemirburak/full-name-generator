907d7fe2a37dcf75bac622d4006fe37414a7c35f
Ryan Biondo <biondoryan.dev@gmail.com>
2023-10-14 15:08:30 -0600

    Added Portuguese names

---
+++ src/names/pt.ts
@@ -0,0 +1,21 @@
+
+const portugueseNames = {
+    0: [ // Male names - https://www.familyeducation.com/, https://www.behindthename.com/, https://forebears.io/, https://www.ine.pt/, https://www.irn.mj.pt/
+        'Afonso', 'Alexandre', 'André', 'António', 'Bernardo', 'Carlos', 'Daniel', 'David', 'Diogo',
+        'Duarte', 'Eduardo', 'Fernando', 'Filipe', 'Francisco', 'Gabriel', 'Gonçalo', 'Gustavo', 'Henrique',
+        'Hugo', 'Ivan', 'João', 'José', 'Leonardo', 'Lucas', 'Luís', 'Manuel', 'Marcos', 'Mário',
+        'Mateus', 'Maurício', 'Miguel', 'Nicolas', 'Nuno', 'Paulo', 'Pedro', 'Rafael', 'Ricardo', 'Rodrigo',
+        'Rui', 'Salvador', 'Samuel', 'Santiago', 'Sérgio', 'Simão', 'Tiago', 'Tomás', 'Vasco', 'Vicente',
+        'Vítor', 'Xavier'
+    ],
+    1: [ // Female names - https://www.familyeducation.com/, https://www.behindthename.com/, https://forebears.io/, https://www.ine.pt/, https://www.irn.mj.pt/
+        'Adriana', 'Alexa', 'Alice', 'Ana', 'Ana Sofia', 'Andreia', 'Bárbara', 'Beatriz', 'Beatriz Silva', 'Bianca',
+        'Carina', 'Carla', 'Carolina', 'Catarina', 'Clara', 'Cláudia', 'Cristina', 'Daniela', 'Diana', 'Diana Catarina',
+        'Diana Sofia', 'Eva', 'Filipa', 'Francisca', 'Gabriela', 'Inês', 'Isabel', 'Jéssica', 'Joana', 'Juliana',
+        'Lara', 'Laura', 'Leonor', 'Letícia', 'Lívia', 'Luana', 'Luna', 'Madalena', 'Margarida', 'Maria Carolina',
+        'Maria Francisca', 'Maria Inês', 'Maria Joana', 'Maria Leonor', 'Maria Luísa', 'Maria Margarida', 'Maria Sofia', 'Marta', 'Mariana', 'Mónica',
+        'Nádia', 'Nicole', 'Olivia', 'Patrícia', 'Sara', 'Sofia', 'Tatiana', 'Vanessa', 'Vera', 'Vitória'
+    ]
+}
+
+export default portugueseNames;
