f3d47fcd4252d378369d7f154f167ad6fb491915
Uday Sagar <udaysagar.mail@gmail.com>
2023-10-14 20:17:06 +0530

    added names and surnames for india

---
+++ src/surnames/in.ts
@@ -0,0 +1,10 @@
+const indianSurnames = [ // https://parenting.firstcry.com/articles/most-popular-indian-surnames-or-last-names-with-meanings/
+    'Singh', 'Devi', 'Patel', 'Kumar', 'Yadav', 'Khan', 'Sharma', 'Gupta', 'Das', 'Reddy',
+    'Rao', 'Ahmed', 'Ali', 'Choudhary', 'Mehra', 'Patel', 'Verma', 'Thakur', 'Saini', 'Singh',
+    'Kapoor', 'Agarwal', 'Mittal', 'Jain', 'Kohli', 'Malik', 'Basu', 'Chatterjee', 'Saha', 'Dasgupta',
+    'Mukherjee', 'Ray', 'Ahmed', 'Khan', 'Pathan', 'Siddiqui', 'Ansari', 'Qureshi', 'Yadav', 'Maurya',
+    'Gupta', 'Sharma', 'Kumar', 'Verma', 'Thakur', 'Singh', 'Kapoor', 'Agarwal', 'Mittal', 'Jain',
+    'Kohli', 'Malik', 'Basu', 'Chatterjee', 'Saha', 'Dasgupta', 'Mukherjee', 'Ray', 'Mehrotra', 'Basak'
+];
+
+export default indianSurnames;
