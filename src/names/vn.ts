9390fca32f63d54fae8f592be5f764af63439e0f
Uday Sagar <udaysagar.mail@gmail.com>
2023-10-14 22:16:29 +0530

    added names and surnames for vietnamese origin

---
+++ src/names/vn.ts
@@ -0,0 +1,18 @@
+const vietnameseNames = {
+    0: [ //https://en.wikipedia.org/wiki/Vietnamese_name
+        'Nguyễn', 'Phúc', 'Thành', 'Dũng', 'Tuấn', 'Hoàng', 'Anh', 'Huy', 'Nam', 'Quang',
+        'Long', 'Hoài', 'Minh', 'Vinh', 'Đạt', 'Trung', 'Khôi', 'Cường', 'Bình', 'Hiếu',
+        'Sơn', 'Khánh', 'Quốc', 'Tài', 'Vân', 'Cảnh', 'Hiểu', 'Nhân', 'Tú', 'Dương',
+        'Hiền', 'Toàn', 'Lộc', 'Hiển', 'Phong', 'Hiệu', 'Sang', 'Thắng', 'Quý', 'Nhật',
+        'An', 'Cú', 'Phát', 'Thương', 'Kiệt', 'Quy', 'Lâm', 'Thiện', 'Phước', 'Tiến'
+    ],
+    1: [ // https://en.wikipedia.org/wiki/Vietnamese_name
+        'Linh', 'Ánh', 'Mai', 'Lan', 'Phương', 'Ngọc', 'Thảo', 'Nga', 'Hương', 'Hoa',
+        'Hiền', 'Hà', 'Thu', 'Hạnh', 'Ngân', 'Thủy', 'Quỳnh', 'Oanh', 'Diệu', 'Yến',
+        'Tuyết', 'Mỹ', 'Lý', 'Phúc', 'Lan Anh', 'Bích', 'Kim', 'Ngọc Ánh', 'Phụng', 'Tuyền',
+        'Vân Ánh', 'Tâm', 'Trang', 'Quyên', 'Thanh Thảo', 'Ngọc Bích', 'Hà Ánh', 'Hạnh Phúc', 'Cẩm', 'Ngọc Trinh',
+        'Hà My', 'Ngọc Hân', 'Tuyết Mai', 'Hồng', 'Ngọc Diệp', 'Ngọc Hạnh', 'Thanh Trúc', 'Huệ', 'Phúc Ánh', 'Thùy Dương'
+    ]
+}
+
+export default vietnameseNames;
