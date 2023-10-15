3e33df2a8fe8c9a4c1b700d4c2722d9bb419ec06
vinayakhegde1 <vinayakhegde619@gmail.com>
2023-10-15 21:03:21 +0530

    Added norwegian names

---
+++ src/surnames/no.ts
@@ -0,0 +1,9 @@
+const norwegianSurnames = [ // Surnames - https://en.wikipedia.org/ , https://www.behindthename.com/
+    'Hansen', 'Johansen', 'Olsen', 'Larsen', 'Andersen', 'Pedersen', 'Nilsen', 'Kristensen', 'Karlsen', 'Eriksen',
+    'Haugen', 'Svendsen', 'Jakobsen', 'Tangen', 'Bakke', 'Strand', 'Solberg', 'Ødegaard', 'Jensen', 'Andreassen',
+    'Berg', 'Sørensen', 'Rasmussen', 'Hagen', 'Moen', 'Thoresen', 'Gundersen', 'Hermansen', 'Johnsen', 'Sletten',
+    'Knutsen', 'Aas', 'Eide', 'Sunde', 'Myhre', 'Rønning', 'Aune', 'Lund', 'Rønningen', 'Solheim', 'Bjørnstad',
+    'Andresen', 'Nygård', 'Holm', 'Tvedt', 'Dahle', 'Sande', 'Bjerke', 'Nygaard', 'Evensen', 'Iversen'
+];
+
+export default norwegianSurnames;
