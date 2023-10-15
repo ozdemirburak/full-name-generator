8c4e7501b3ad51cca7a4439068a95fa15ba212f2
RileeyL <146129446+RileeyL@users.noreply.github.com>
2023-10-15 11:24:36 -0700

    Create th.ts
    Added most common Thai surnames

---
+++ src/surnames/th.ts
@@ -0,0 +1,9 @@
+const thaiSurnames = [ // Surnames - https://surnam.es/thailand
+    'Saetang', 'Saelim', 'Saeli', 'Bunmi', 'Sukkasem', 'Saeueng', 'Thongkham', 'Sangthong', 'Chanthara', 'Suwan',
+    'Thongsuk', 'Sichantha', 'Photsi', 'Chaichana', 'Sisuk', 'Bunma', 'Ayutthaya', 'Saetan', 'Saengthong', 'Sitwat',
+    'Thongdi', 'Bunsi', 'Sopha', 'Saithong', 'Kongkaeo', 'Panya', 'Rungrueang', 'Sombun', 'Hongthong', 'Bunrueang',
+    'Thongsi', 'Panthong', 'Saengsawang', 'Chaiwong', 'Surname1', 'Sisuwan', 'Surname1', 'Chairat', 'Ketukaeo', 'Buathong',
+    'Wongsa', 'Bunloet', 'Chaimongkhon', 'Sukhotwat', 'Saelao', 'Saetiao', 'Phasuk', 'Bunchu', 'Photthong', 'Sithong',
+];
+
+export default thaiSurnames;
