0f52d30d649e374d7436b29cb394403b178847cc
Anemaygi <gianemayumisan@gmail.com>
2023-10-15 09:57:39 -0300

    add finnish surnames

---
+++ src/surnames/fi.ts
@@ -0,0 +1,9 @@
+const finnishSurnames = [ // https://www.avoindata.fi/data/en_GB/dataset/none
+    'Korhonen','Virtanen','Mäkinen','Nieminen','Mäkelä','Hämäläinen','Laine','Heikkinen','Koskinen','Järvinen',
+    'Lehtonen','Lehtinen','Saarinen','Salminen','Heinonen','Heikkilä','Niemi','Kinnunen','Salonen','Turunen',
+    'Salo','Laitinen','Rantanen','Tuominen','Karjalainen','Mattila','Jokinen','Savolainen','Lahtinen','Ahonen',
+    'Ojala','Leppänen','Kallio','Leinonen','Väisänen','Hiltunen','Pitkänen','Miettinen','Manninen','Aaltonen',
+    'Koivisto','Hakala','Anttila','Laaksonen','Hirvonen','Lehto','Räsänen','Laakso','Toivonen','Rantala',
+];
+
+export default finnishSurnames;
