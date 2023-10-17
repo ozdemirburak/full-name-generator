dd060be25ff027d6c8dc2491bd87eb9277da1039
Arkhein <richmondnkrumah04@gmail.com>
2023-10-17 15:43:27 +0000

    Added Japanese names (#36)
    added japanese names

---
+++ src/surnames/jp.ts
@@ -0,0 +1,10 @@
+const japaneseSurnames = [ 
+    // https://en.wikipedia.org/wiki/Category:Japanese-language_surnames
+    'Adachi', 'Aiso','Asagiri', 'Akada','Akane','Amuro','Chidori','Chigusa','Chisaka','Dobayashi',
+    'Ebisu','Egami','Esashi','Ezumi','Emon','Fubuki','Fueki','Fujihara','Fujikawa','Fukikoshi',
+    'Futaba','Futara','Hachisuka','Hagihara','Hagimoto','Hagimura','Hachisu','Hagino','Maeda','Machino',
+    'Maehara','Maeno','Maeshiro','Makishima','Makiyama','Marumo','Maruya','Matayoshi','Mitsuda','Miyama',
+    'Takahashi','Takagi','Takamiya','Takamori','Takami','Takamoto','Takamura','Taneda','Wakahara','Ushioda'
+];
+
+export default japaneseSurnames;
