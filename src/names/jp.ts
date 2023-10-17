e4847ebe7b94f8585ae7f033de3dcee071ac8858
Arkhein <richmondnkrumah04@gmail.com>
2023-10-17 01:22:11 +0000

    Added Japanes names

---
+++ src/names/jp.ts
@@ -0,0 +1,18 @@
+const japaneseNames = {
+    0: [ // https://en.wikipedia.org/wiki/Category:Japanese_masculine_given_names
+        'Asahi','Atomu','Atsuji','Atsuya','Arata','Akiya','Atsuo','Banri','Atsuhiko','Chikashi',
+        'Daiki','Daiji','Eisuke','Enji','Eiken','Etsuji','Fukashi','Fuyuki','Fusao','Fuyuki',
+        'Gakuto','Genta','Gota','Hajime','Haruki','Haruhiro','Haruhiko','Hibiki','Haruto','Heisuke',
+        'Hideshi','Hiroshi','Inasa','Inosuke','Itsuo','Junata','Junpei','Kaito','Kagemori','Kaisei',
+        'Kanehiro','Katsu','Katsuya','Mahiro','Makito','Manabu','Mahito','Masamura','Naofumi','Nagatoki'
+    ],
+    1: [ // https://en.wikipedia.org/wiki/Category:Japanese_feminine_given_names
+        'Atsuko','Asaka','Asagiri','Ariko','Ayami','Asumi','Chiho','Chizuru','Chiyako','Eiko','Fumi',
+        'Ayane','Haruyo','Hasumi','Hibari','Haruno','Hinako','Hiroe','Hinako','Inori','Itsumi',
+        'Kaede','Kahori','Kaori','Kanoko','kimiko','Kotori','Maika','Makiko','Manaka','Megumi',
+        'Michi','Midori','Mikako','Nagako','Nanami','Naru','Natsue','Nonoka','Noriyo','Reiko',
+        'Rika','Risako','Rumiko','Ruri','Sayuki','Saori','Satomi','Sanami','Sayumi','Setsuko'
+    ]
+}
+
+export default japaneseNames;
