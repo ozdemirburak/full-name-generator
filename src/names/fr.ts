2b719928c83325ca3830df0505391a5e91169d2e
Ons Belkhouja <48824008+belkhoujaons@users.noreply.github.com>
2023-10-17 11:43:32 +0100

    add french names of males and females and surnames (#33)
    added french names

---
+++ src/names/fr.ts
@@ -0,0 +1,18 @@
+const frenchNames = {
+  0: [ // 2000 ~ 2022 - https://web.archive.org/web/20231016211828/https://www.insee.fr/fr/outil-interactif/3532172/xml/Top100prenoms.xml
+      'Lucas', 'Hugo', 'Enzo', 'Louis', 'Nathan', 'Théo', 'Gabriel', 'Léo', 'Thomas', 'Jules', 
+      'Arthur', 'Raphaël', 'Mathis', 'Tom', 'Maxime', 'Paul', 'Clément', 'Adam', 'Antoine', 'Ethan', 
+      'Noah', 'Alexandre', 'Baptiste', 'Axel', 'Yanis', 'Alexis', 'Valentin', 'Mathéo', 'Sacha', 'Quentin', 
+      'Mohamed', 'Nolan', 'Victor', 'Maxence', 'Gabin', 'Evan', 'Romain', 'Liam', 'Nicolas', 'Martin', 
+      'Rayan', 'Samuel', 'Maël', 'Simon', 'Benjamin', 'Robin', 'Adrien', 'Pierre', 'Julien', 'Mattéo', 
+  ],
+  1: [ // 2000 ~ 2022 - https://web.archive.org/web/20231016211828/https://www.insee.fr/fr/outil-interactif/3532172/xml/Top100prenoms.xml
+      'Emma', 'Léa', 'Manon', 'Chloé', 'Jade', 'Camille', 'Louise', 'Inès', 'Sarah', 'Clara', 
+      'Lola', 'Lucie', 'Eva', 'Alice', 'Lina', 'Zoé', 'Juliette', 'Marie', 'Anaïs', 'Léna', 
+      'Romane', 'Océane', 'Ambre', 'Mathilde', 'Jeanne', 'Lou', 'Lilou', 'Charlotte', 'Lisa', 'Anna', 
+      'Laura', 'Julie', 'Pauline', 'Julia', 'Louna', 'Clémence', 'Margaux', 'Rose', 'Nina', 'Célia', 
+      'Agathe', 'Justine', 'Alicia', 'Margot', 'Elisa', 'Léonie', 'Mila', 'Elise', 'Luna', 'Emilie',
+  ]
+};
+
+export default frenchNames;
---
+++ src/surnames/fr.ts
@@ -0,0 +1,9 @@
+const frenchSurnames = [ // https://web.archive.org/web/20231016213609/https://fr.wikipedia.org/wiki/Liste_des_noms_de_famille_les_plus_courants_en_France
+    'Martin', 'Bernard', 'Thomas', 'Petit', 'Robert', 'Richard', 'Durand', 'Dubois', 'Moreau', 'Laurent', 
+    'Simon', 'Michel', 'Lefebvre', 'Leroy', 'Roux', 'David', 'Bertrand', 'Morel', 'Fournier', 'Girard', 
+    'Bonnet', 'Dupont', 'Lambert', 'Fontaine', 'Rousseau', 'Vincent', 'Muller', 'Lefevre', 'Faure', 'Andre', 
+    'Mercier', 'Blanc', 'Guerin', 'Boyer', 'Garnier', 'Chevalier', 'Francois', 'Legrand', 'Gauthier', 'Garcia', 
+    'Perrin', 'Robin', 'Clement', 'Morin', 'Nicolas', 'Henry', 'Roussel', 'Mathieu', 'Gautier', 'Masson', 
+];
+
+export default frenchSurnames;
