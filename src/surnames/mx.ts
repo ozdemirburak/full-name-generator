4c15d22fac08b842afa555b2a8a70a919c15bd5f
Gustavo Almeida de Jesus <66255218+SystemGuuh@users.noreply.github.com>
2023-10-20 05:30:09 -0300

    add mexican names and surnames (#38)

---
+++ src/surnames/mx.ts
@@ -0,0 +1,9 @@
+const mexicanSurnames = [ // https://www.telediario.mx/comunidad/cuales-son-los-apellidos-mas-comunes-en-mexico
+    'Hernández', 'Martínez', 'López', 'González', 'Pérez', 'Rodríguez', 'Sánchez', 'Ramírez', 'Cruz', 'Gómez',
+    'Flores', 'Morales', 'Vázquez', 'Jiménez', 'Reyes', 'Díaz', 'Torres', 'Gutiérrez', 'Ruiz', 'Mendoza',
+    'Aguilar', 'Méndez', 'Moreno', 'Ortíz', 'Juárez', 'Castillo', 'Romero', 'Ramos', 'Rivera', 'Chávez', 
+    'De la Cruz', 'Domínguez', 'Guzmán', 'Velázquez', 'Santiago', 'Herrera', 'Castro', 'Vargas', 'Medina', 
+    'Rojas', 'Muñóz', 'Luna', 'Contreras', 'Bautista', 'Salazar', 'Ortega', 'Guerrero', 'Estrada'
+];
+
+export default mexicanSurnames;
