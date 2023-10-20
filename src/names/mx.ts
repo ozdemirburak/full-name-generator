288bd48ca2ee24f99a126f4ffff0693fd2dad0e7
Gustavo Almeida <gustavo.jesus2806@gmail.com>
2023-10-19 23:32:46 -0300

    add mexican names and surnames

---
+++ src/names/mx.ts
@@ -0,0 +1,18 @@
+const mexicanNames = {
+    0: [ // https://datos.cdmx.gob.mx/dataset/nombres-registrados-en-actas-del-registro-civil/resource/4860083b-f893-4043-9344-bd8602db156a
+        'Hugo', 'Mateo', 'Martín', 'Lucas', 'Leo', 'Daniel', 'Alejandro', 'Manuel', 'Pablo', 'Álvaro',
+        'Adrián', 'Enzo', 'Mario', 'Diego', 'David', 'Oliver', 'Marcos', 'Thiago', 'Marco', 'Alex',
+        'Javier', 'Izan', 'Bruno', 'Miguel', 'Antonio', 'Gonzalo', 'Liam', 'Gael', 'Marc', 'Carlos',
+        'Juan', 'Angel', 'Dylan', 'Nicolás', 'Jose', 'Sergio', 'Gabriel', 'Luca', 'Jorge', 'Dario',
+        'Iker', 'Samuel', 'Eric', 'Adam', 'Hector', 'Francisco', 'Rodrigo', 'Jesus', 'Erik','Amir'
+    ],
+    1: [ // https://datos.cdmx.gob.mx/dataset/nombres-registrados-en-actas-del-registro-civil/resource/4860083b-f893-4043-9344-bd8602db156a
+        'Lucía', 'Sofia', 'Martina', 'María', 'Julia', 'Paula', 'Valeria', 'Emma', 'Daniela', 'Carla',
+        'Alba', 'Noa', 'Alma', 'Sara', 'Carmen', 'Vega', 'Lara', 'Mia', 'Valentina', 'Olivia',
+        'Claudia', 'Jimena', 'Lola', 'Chloe', 'Aitana', 'Abril', 'Ana', 'Laia', 'Triana', 'Alejandra',
+        'Elena', 'Vera', 'Manuela', 'Adriana', 'Ines', 'Marta', 'Carlota', 'Irene', 'Victoria', 'Blanca',
+        'Marina', 'Laura', 'Rocío', 'Alicia', 'Clara', 'Nora', 'Lia', 'Ariadna','Zoe','Amira'
+    ]
+};
+
+export default mexicanNames;
