970d742bc511551deececae922b6553940fc259e
Miguel Puga <86690808+MiguelPuga@users.noreply.github.com>
2023-10-28 15:17:28 -0300

    added peruvian names.

---
+++ src/names/pe.ts
@@ -0,0 +1,18 @@
+const peruvianNames = {
+    0: [ // https://www.losandes.com.pe/oweb/Nacional/20150616/89321.html
+        'José', 'Juan', 'Luis', 'Carlos', 'Jorge', 'Víctor', 'César', 'Miguel', 'Julio', 'Jesús',
+        'Pedro', 'Manuel', 'Santos', 'Jhon', 'David', 'Diego', 'Segundo', 'Daniel', 'Óscar', 'Marco',
+        'Edwin', 'Javier', 'Walter', 'Francisco', 'Fernando', 'Milagros', 'Alejandro', 'Raul', 'Ricardo', 'Alex',
+        'Roberto', 'Edgar', 'Eduardo', 'Mario', 'Jaime', 'Felix', 'Hector', 'Pablo', 'Kevin', 'Alexander',
+        'Jean', 'Rocio', 'Cristian', 'Antonio', 'Alberto', 'Hugo', 'Sebastian', 'Andres', 'Percy', 'Christian',
+    ],
+    1: [ // https://www.losandes.com.pe/oweb/Nacional/20150616/89321.html
+        'María', 'Rosa', 'Ana', 'Luz', 'Carmen', 'Juana', 'Ángel', 'Julia', 'Flor', 'Elizabeth',
+        'Ruth', 'Diana', 'Victoria', 'Gloria', 'Andrea', 'Nancy', 'Gladys', 'Sonia', 'Isabel', 'Martha',
+        'Claudia', 'Silvia', 'Margarita', 'Sandra', 'Patricia', 'Yolanda', 'Norma', 'Jessica', 'Mercedes', 'Olga',
+        'Angela', 'Edith', 'Lucia', 'Karen', 'Roxana', 'Vilma', 'Hilda', 'Lidia', 'Erika', 'Teresa',
+        'Alicia', 'Camila', 'Nelly', 'Karina', 'Maribel', 'Khaterine', 'Elsa', 'Luisa', 'Doris', 'Ariana',
+    ]
+};
+
+export default peruvianNames;
